9c66ff135195fad6eaaded21f8263959262ba0d0
Jimmy Rivera <jimmy.rivera1@unmsm.edu.pe>
2023-05-08 19:02:49 -0500

    Agregado formulario base

--- control_estudios/urls.py
+++ control_estudios/urls.py
@@ -21,4 +21,5 @@ from control_estudios.views import *
 urlpatterns = [
     path("estudiantes/", listar_estudiantes, name="lista_estudiantes"),
     path("cursos/", listar_cursos, name="lista_cursos"),
+    path("crear-curso/",crear_curso,name="crear-curso"),
 ]
--- control_estudios/views.py
+++ control_estudios/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-def listar_estudiantes(request):
+from control_estudios.models import *
+# Create your views here.   
+def listar_estudiantes(request):    
     contexto={
-         "estudiantes":[
-             {"nombre":"Jimmy","apellido":"Rivera"},
-             {"nombre":"Jorge","apellido":"Torres"},
-             {"nombre":"Toribia","apellido":"Matos"},
-             {"nombre":"Jenifer","apellido":"Bujele"},
-             {"nombre":"Ibonne","apellido":"Tenorio"},
-             {"nombre":"Luisa","apellido":"lberto"},
-             ]              
+         "estudiantes":Estudiante.objects.all(),         
     }
     http_responde =render(
         request=request,
@@ -20,15 +13,19 @@ def listar_estudiantes(request):
     return http_responde
 def listar_cursos(request):
     contexto={
-         "cursos":[
-             {"nombre":"Python","comision":"40440"},
-             {"nombre":"Data Science","comision":"50550"},
-             {"nombre":"Looker Studio with Google Analytics","comision":"60660"},
-             ]              
+         "cursos":Curso.objects.all(),            
     }
     http_responde =render(
         request=request,
         template_name='control_estudios/lista_cursos.html',
+        context=contexto,
+    )
+    return http_responde
+def crear_curso(request):
+    contexto={}
+    http_responde =render(
+        request=request,
+        template_name='control_estudios/formulario_curso_a_mano.html',
         context=contexto,
     )
     return http_responde
